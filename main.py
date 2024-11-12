f81800359204f85f95a8c5835bb3caf1c6f64289
XeenProof <whosejimmy@gmail.com>
2024-11-12 10:17:00 -0500

    main file reader completed

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from pathlib import Path
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from schemaGenerator import generateGeoSegmentSchema, generateSegment1Schema, generateSegment2Schema, generateSegment3Schema
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+main_path = os.getenv('FOLDER_PATH', '')
 
 spark_builder: SparkSession.Builder = SparkSession.builder
 spark_builder.appName("p3-census").config("spark.master", "local[*]")
@@ -7,11 +14,9 @@ spark_builder.appName("p3-census").config("spark.master", "local[*]")
 spark: SparkSession = spark_builder.getOrCreate()
 sc = spark.sparkContext
 
-geoheader = spark.read.csv("p3/Alabama/algeo2020.pl", sep='|')
-file1 = spark.read.csv("p3/Alabama/al000012020.pl", sep='|')
+geo_df = spark.read.csv(f'{main_path}/GeoHeader', sep='|', schema=generateGeoSegmentSchema())
+seg1_df = spark.read.csv(f'{main_path}/Segment1', sep='|', schema=generateSegment1Schema())
+seg2_df = spark.read.csv(f'{main_path}/Segment2', sep='|', schema=generateSegment2Schema())
+seg3_df = spark.read.csv(f'{main_path}/Segment3', sep='|', schema=generateSegment3Schema())
+print(seg2_df.filter(col(geo_df.columns[1])=='US').head())
 
-with open("NotesToSelf/output.txt", "w") as file:
-     file.write(file1._show_string())
-file1.printSchema()
-
-spark.stop()
