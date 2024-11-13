cfe3b7cdacd56ad35649b92c15c964f45b15c8e3
XeenProof <whosejimmy@gmail.com>
2024-11-13 10:31:42 -0500

    years separated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 from schemaGenerator import generateGeoSegmentSchema, generateSegment1Schema, generateSegment2Schema, generateSegment3Schema
+from pygen_census import gen_schema
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -19,13 +20,15 @@ seg1_df = spark.read.csv(f'{main_path}/p3_data_2020/Segment1', sep='|', schema=g
 seg2_df = spark.read.csv(f'{main_path}/p3_data_2020/Segment2', sep='|', schema=generateSegment2Schema())
 seg3_df = spark.read.csv(f'{main_path}/p3_data_2020/Segment3', sep='|', schema=generateSegment3Schema())
 
-#segmented 
+geo_df_2010 = gen_schema(spark.read.text(f"{main_path}/p3_data_2010/GeoHeader"), spark, 2010)
 seg1_df_2010 = spark.read.csv(f'{main_path}/p3_data_2010/Segment1', sep='|', schema=generateSegment1Schema())
 seg2_df_2010 = spark.read.csv(f'{main_path}/p3_data_2010/Segment2', sep='|', schema=generateSegment1Schema())
 
+geo_df_2000 = gen_schema(spark.read.text(f"{main_path}/p3_data_2000/GeoHeader"), spark, 2000)
 seg1_df_2000 = spark.read.csv(f'{main_path}/p3_data_2000/Segment1', sep='|', schema=generateSegment1Schema())
 seg2_df_2000 = spark.read.csv(f'{main_path}/p3_data_2000/Segment2', sep='|', schema=generateSegment1Schema())
 
+geo_df_2000.show()
 # print(seg2_df.filter(col(geo_df.columns[1])=='US').head())
 # seg1_df_2000.show()
 
