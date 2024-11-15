03e673886c1ea0ec55a64aa6d053fb54fc3aaea7
Dejen <dejenaref@gmail.com>
2024-11-15 13:44:48 -0500

    schema printed for 2000

--- dej_questions_analysis/analysis.py
+++ dej_questions_analysis/analysis.py
@@ -2,4 +2,13 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("CensusParquetAnalysis").getOrCreate()
 
-base_path = "hdfs:///user/dejtes/census_data_parquet"
+# Paths to HDFS directories
+base_path = "hdfs:///user/dirname/census_data_parquet"
+
+df_2000 = spark.read.parquet(f"{base_path}/YEAR=2000")
+
+# Print schema
+df_2000.printSchema()
+
+df_2000.show(5)
+
