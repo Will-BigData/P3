d295d0840b8297d7ed334fdd5b3306003c0536b0
michaelmengistu2 <michaelmengistu.mm@gmail.com>
2024-11-18 10:22:35 -0600

    updating my text files and python file

--- michael_data_analysis/census_analysis_spark.py
+++ michael_data_analysis/census_analysis_spark.py
@@ -16,9 +16,15 @@ names = rdd.collect()
 #remove first element
 names.pop(0)
 
-#2000 year census path
-path_2000 = "/myp3/input/p3_output/YEAR=2000" 
+#2000 year census path from the 
+path_2000 = "/myp3/input/original_p3_data/YEAR=2000" 
 
 #read all parquet files in the folder
 df = spark.read.parquet(path_2000)
 
+
+#show first in data frame 
+df.show(1)
+
+#print data frame schema
+df.printSchema()
